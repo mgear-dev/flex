2c4d29f687b5247741224b7246563f7b44bf30a2
Jerome Drese <jdrese@gmail.com>
2018-06-13 20:26:51 +0200

    Add flex.update module
    This module will be the module responsible on executing the update

---
+++ python/flex/update.py
@@ -0,0 +1,26 @@
+
+""" flex.update
+
+flex.update module handles the updating rig process
+
+:module: flex.update
+"""
+
+# imports
+from .compare import get_updatable_shapes
+
+
+def update_rig(source, target):
+    """ Updates all shapes from the given source group to the target group
+
+    :param source: maya transform node
+    :type group: string
+
+    :param target: maya transform node
+    :type group: string
+
+    :return: failed and succeeded updated shapes
+    :rtype: dict
+    """
+
+    return get_updatable_shapes(source, target)
