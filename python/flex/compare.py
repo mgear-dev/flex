95176585d9f1ba35cc8278668de250bf4dcfd73f
Jerome Drese <jdrese@gmail.com>
2018-06-13 16:15:02 +0200

    Add get_prefix_from_group
    This returns the prefix (namespace) that a given maya transform group can have

--- python/flex/compare.py
+++ python/flex/compare.py
@@ -9,6 +9,23 @@
 from maya import cmds
 
 
+def get_prefix_from_group(group):
+    """ Returns a string with the prefix found on the given group
+
+    :param group: maya transform node
+    :type group: string
+
+    :return: the group prefix
+    :rtype: str
+    """
+
+    if not group.count(':'):
+        return None
+
+    end_name = group.split(':')[-1]
+    return group.replace(end_name, '')
+
+
 def get_shapes_from_group(group):
     """ Gets all object shapes existing inside the given group
 
