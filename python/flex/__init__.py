2a478a7e0783e16f28585270381dacbda37dd7a6
Jerome Drese <jdrese@gmail.com>
2018-06-13 15:57:12 +0200

    add flex python package
    Simple init for the flex python package

---
+++ python/flex/__init__.py
@@ -0,0 +1,11 @@
+""" **FLEX**
+
+Flex is the mGear models (geometry) update tool inside rigs.
+
+
+:module: flex.__init__
+"""
+
+__author__ = 'Jerome Drese'
+__email__ = 'jeromed@gmail.com'
+__version__ = '1.0.0'
