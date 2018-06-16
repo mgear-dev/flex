685f2d92dd90b2fe00e30f750671028487fa385d
Jerome Drese <jdrese@gmail.com>
2018-06-16 15:06:34 +0200

    Add execution for updating each shape
    update_rig handles finding the matching shapes and will trigger the update on each one of them individually

--- python/flex/update.py
+++ python/flex/update.py
@@ -19,8 +19,26 @@ def update_rig(source, target):
     :param target: maya transform node
     :type group: string
 
-    :return: failed and succeeded updated shapes
-    :rtype: dict
+    :param constant_prefix: all shapes on source have a constant prefix
+    :type constant_prefix: bool
     """
 
-    return get_updatable_shapes(source, target)
+    # gets the shapes from source and target
+    source_shapes = get_shapes_from_elem(source)
+    target_shapes = get_shapes_from_elem(target)
+
+    # gets prefix from source
+    prefix = get_prefix_from_elem(source)
+
+    for shape in source_shapes:
+        target_name = shape
+
+        if not constant_prefix:
+            prefix = get_prefix_from_elem(shape)
+
+        target_name = shape.replace(prefix, '')
+
+        if target_name not in target_shapes:
+            continue
+
+        update_shape(shape, target_name)
