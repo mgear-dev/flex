ba7f42d2790dc3f4364ee5ed19529b1149168c33
Jerome Drese <jdrese@gmail.com>
2018-06-16 15:07:28 +0200

    Add update_shape method declaration
    update_shape is the method that handles the update on the given source and target shape objects

--- python/flex/update.py
+++ python/flex/update.py
@@ -7,10 +7,14 @@ flex.update module handles the updating rig process
 """
 
 # imports
-from flex.query import get_updatable_shapes
+from flex.query import get_prefix_from_elem, get_shapes_from_elem
 
 
-def update_rig(source, target):
+def update_shape(source_shape, target_shape):
+    print source_shape, target_shape
+
+
+def update_rig(source, target, constant_prefix):
     """ Updates all shapes from the given source group to the target group
 
     :param source: maya transform node
