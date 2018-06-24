adf5275a94d987b3c46b1647944135f4a735f5c3
Jerome Drese <jdrese@gmail.com>
2018-06-24 09:40:54 +0200

    Speed up shapes matching method
    Previous version was a ruff test that was giving slow results when trying to find matching names on big lists. The newer version uses dict to find the matching shapes and this is instantaneously

--- python/flex/query.py
+++ python/flex/query.py
@@ -8,37 +8,93 @@ of maya transform nodes used as groups.
 
 # imports
 from maya import cmds
+from flex.decorators import timer  # @UnusedImport
 
 
-def get_prefix_from_elem(elem):
-    """ Returns a string with the prefix found on the given group
+@timer
+def get_matching_shapes(source_shapes, target_shapes):
+    """ Returns the matching shapes
 
-    :param elem: maya transform node
-    :type elem: string
+    This Function will return a dict that contains the target matching shape
+    name from the source.
 
-    :return: the group prefix
-    :rtype: str
+    :param source_shapes: sources dictionary containing prefix-less shapes
+    :type group: dict
+
+    :param target: targets dictionary containing prefix-less shapes
+    :type group: dict
+
+    :return: The matching target shapes names
+    :rtype: dict
+
+    .. note:: This function is the core idea of how Flex finds matching shapes
+              from a source group to the target. Because Flex is not part of a
+              specific studio pipeline this matching is **shapes name based**.
+              Because some studios might bring the source scene into the rig
+              scene as a reference or as an import we cover those two cases.
+
+              Using this dict is the fastest way (so far found) to deal with
+              a huge amount of names. Finding the matching names on a scene
+              with more than 2000 shapes takes 0.0009... seconds.
     """
 
-    end_name = None
-
-    # namespace case prefix
-    if elem.count(':'):
-        end_name = elem.split(':')[-1]
-
-    # double name case prefix
-    if elem.count('|'):
-        end_name = elem.split('|')[-1]
-
-    if end_name:
-        return elem.replace(end_name, '')
+    # returns matching shapes
+    return dict([(source_shapes[s], target_shapes[s])
+                 for s in source_shapes
+                 if s in target_shapes])
 
 
-def get_shapes_from_elem(elem):
+@timer
+def get_missing_shapes(source_shapes, target_shapes):
+    """ Returns the missing shapes
+
+    This Function will return a dict that contains the missing shape
+    found on the target.
+
+    :param source_shapes: sources dictionary containing prefix-less shapes
+    :type group: dict
+
+    :param target: targets dictionary containing prefix-less shapes
+    :type group: dict
+
+    :return: The missing target shapes names
+    :rtype: dict
+    """
+
+    # returns matching shapes
+    return dict([(source_shapes[s], s)
+                 for s in source_shapes
+                 if s not in target_shapes])
+
+
+def get_prefix_less_dict(elements):
+    """ Returns a dict containing each element with a stripped prefix
+
+    This Function will return a dict that contains each element resulting on
+    the element without the found prefix
+
+    :param elements: List of all your shapes
+    :type group: list
+
+    :return: The matching prefix-less elements
+    :rtype: dict
+
+    .. note:: Because Flex is not part of a specific studio pipeline we cover
+              two different ways to bring the source shapes inside your rig.
+              You can either import the source group with the meshes or use
+              a Maya reference. This function will strip the prefix whether
+              your object is part of a namespace or a double name getting a
+              full path naming.
+    """
+
+    return dict([(n.split("|")[-1].split(":")[-1], n) for n in elements])
+
+
+def get_shapes_from_group(group):
     """ Gets all object shapes existing inside the given group
 
-    :param elem: maya transform node
-    :type elem: string
+    :param group: maya transform node
+    :type group: string
 
     :return: list of shapes objects
     :rtype: list str
@@ -47,11 +103,11 @@ def get_shapes_from_elem(elem):
     """
 
     # checks if exists inside maya scene
-    if not cmds.objExists(elem):
-        raise RuntimeError('Given element {} does not exists.'.format(elem))
+    if not cmds.objExists(group):
+        raise RuntimeError('Given element {} does not exists.'.format(group))
 
     # gets shapes inside the given group
-    shapes = cmds.ls(elem, dagObjects=True, noIntermediate=True,
+    shapes = cmds.ls(group, dagObjects=True, noIntermediate=True,
                      exactType=('mesh'))
 
     return shapes or None
