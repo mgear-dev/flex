8e6778f5460424a2ae5fabc64e481c9eb2d27f92
Jerome Drese <jdrese@gmail.com>
2018-06-29 12:00:14 +0200

    Add deforme shapes update process
    Triggers the orig shape update and the user define attributes update

--- python/flex/update.py
+++ python/flex/update.py
@@ -8,22 +8,25 @@ flex.update module handles the updating rig process
 
 # imports
 import logging
-from flex.query import get_matching_shapes, get_shapes_from_group, get_prefix_less_dict, \
-    get_missing_shapes
+from maya import cmds
+from .decorators import timer
+from .query import (get_matching_shapes, get_shapes_from_group,
+                    get_prefix_less_dict, get_missing_shapes,
+                    get_shape_orig)
 
-logger = logging.getLogger('mGear: Flex')
+logger = logging.getLogger("mGear: Flex")
 logger.setLevel(logging.DEBUG)
 
 
-# @timer
+@timer
 def update_rig(source, target):
     """ Updates all shapes from the given source group to the target group
 
     :param source: maya transform node
-    :type group: string
+    :type group: str
 
     :param target: maya transform node
-    :type group: string
+    :type group: str
     """
 
     # gets all shapes on source and target
@@ -43,10 +46,89 @@ def update_rig(source, target):
     # gets missing source shapes
     missing_source_shapes = get_missing_shapes(targets_dict, sources_dict)
 
-    logger.info(matching_shapes)
-    logger.info(missing_source_shapes)
-    logger.info(missing_target_shapes)
+    logger.info("Matching shapes: {}" .format(matching_shapes))
+
+    for shape in matching_shapes:
+        logger.info("Updating: {}".format(shape))
+        update_shape(shape, matching_shapes[shape])
+        update_shape_user_attributes(shape, matching_shapes[shape])
+
+    logger.info("Source missing shapes: {}" .format(missing_source_shapes))
+    logger.info("Target missing shapes: {}" .format(missing_target_shapes))
 
 
 def update_shape(source_shape, target_shape):
-    return source_shape, target_shape
+    """ Updates the target shape with the given source shape content
+
+    :param source_shape: maya shape node
+    :type source_shape: str
+
+    :param target_shape: maya shape node
+    :type target_shape: str
+    """
+
+    deform_origin = get_shape_orig(target_shape)
+
+    if not deform_origin:
+        return
+
+    deform_origin = deform_origin[0]
+
+    # updates the shape
+    cmds.connectAttr("{}.outMesh".format(source_shape),
+                     "{}.inMesh".format(deform_origin), force=True)
+
+    # forces shape evaluation to achieve the update
+    cmds.dgeval("{}.outMesh".format(target_shape))
+
+    # finish shape update
+    cmds.disconnectAttr("{}.outMesh".format(source_shape),
+                        "{}.inMesh".format(deform_origin))
+
+
+def update_shape_user_attributes(source_shape, target_shape):
+    """ Updates the target shape attributes with the given source shape content
+
+    :param source_shape: maya shape node
+    :type source_shape: str
+
+    :param target_shape: maya shape node
+    :type target_shape: str
+    """
+
+    # loop on user defined attributes if any
+    for attr in cmds.listAttr(source_shape, userDefined=True) or []:
+
+        # get attribute type and value
+        attr_type = cmds.getAttr(source_shape + "." + attr, type=True)
+        attr_value = cmds.getAttr(source_shape + "." + attr)
+
+        # checks if attribute exists on target shape
+        if not cmds.objExists(target_shape + '.' + attr):
+            # handles attribute type attributes
+            try:
+                cmds.addAttr(target_shape, longName=attr,
+                             attributeType=attr_type)
+
+            # handles data type attributes
+            except RuntimeError:
+                cmds.addAttr(target_shape, longName=attr,
+                             dataType=attr_type)
+
+        # updates the attribute values
+        try:
+            # handles most general attribute cases
+            cmds.setAttr(source_shape + "." + attr, attr_value)
+
+        except RuntimeError:
+            # handles array type attributes
+            # .. todo:: Support all types of Maya attributes
+            if 'Array' in attr_type:
+                cmds.setAttr(target_shape + "." + attr, len(attr_value),
+                             *((x, y, z) for x, y, z in attr_value),
+                             type=attr_type)
+
+            # handles extra attribute types
+            else:
+                cmds.setAttr(target_shape + "." + attr, attr_value,
+                             type=attr_type)
