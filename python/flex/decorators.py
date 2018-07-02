a83a1735c039beeb70e7c4e239bda86b2a33d56c
Jerome Drese <jerome.dr@grid-vfx.com>
2018-07-02 18:24:34 +0200

    Add kill flex window ui decorato

--- python/flex/decorators.py
+++ python/flex/decorators.py
@@ -9,6 +9,53 @@ decorators.
 
 # imports
 import time
+from maya import cmds
+
+
+def kill_flex_ui(function):
+    """ Kill an already existing Flex ui
+
+    Checks if a existing instance of the Flex UI is open and kills it.
+
+    :param function: your decorated function
+    :type function: function
+
+    :return: your decorated function
+    :rtype: function
+    """
+
+    # checks for Flex ui instance
+    if cmds.window("flex_qdialog", exists=True):
+        cmds.deleteUI("flex_qdialog")
+
+    def wrapper_function(*args, **kwars):
+        # runs decorated function
+        function_exec = function(*args, **kwars)
+
+        return function_exec
+
+    return wrapper_function
+
+
+def set_focus(function):
+    """ Set focus on Flex window
+
+    Sets focus on Flex UI.
+
+    :param function: your decorated function
+    :type function: function
+
+    :return: your decorated function
+    :rtype: function
+    """
+
+    def wrapper_function(*args, **kwars):
+        # runs decorated function
+        function_exec = function(*args, **kwars)
+        cmds.setFocus("flex_qdialog")
+        return function_exec
+
+    return wrapper_function
 
 
 def timer(function):
