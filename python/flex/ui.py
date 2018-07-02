90159f122d6088975d4fc504e8fb7b1aea4c5503
Jerome Drese <jerome.dr@grid-vfx.com>
2018-07-02 17:26:46 +0200

    Split widgets creation under defined parts
    In order to mantain the ui we split the different parts of the
    ui in different methods

--- python/flex/ui.py
+++ python/flex/ui.py
@@ -24,23 +24,44 @@ class FlexDialog(QtWidgets.QDialog):
         """
         super(FlexDialog, self).__init__(parent=parent)
 
+        # sets window rules
         self.setObjectName('flex_qdialog')
         self.setWindowTitle("mGear: Flex (rig updater)")
         self.setFixedSize(300, 100)
 
-        # ---------------------------------------------------------------------
-        #                       creates widgets
-        # ---------------------------------------------------------------------
+        # creates widgets
+        self.create_layout_widgets()
+        self.create_source_target_widgets()
+        self.create_run_widgets()
+        self.layout_widgets()
+
+    def create_layout_widgets(self):
+        """ Creates the general UI layouts
+        """
 
         # layout widgets
         self.grid_layout = QtWidgets.QGridLayout()
         self.grid_layout.setContentsMargins(5, 5, 5, 5)
         self.setLayout(self.grid_layout)
 
+    def create_run_widgets(self):
+        """ Creates the run widgets area
+        """
+
+        # colors
+        red = QtGui.QColor(250, 110, 90)
+
+        # run widget
+        self.run_button = QtWidgets.QPushButton('--> Update shapes <--')
+        self.run_button.setPalette(red)
+
+    def create_source_target_widgets(self):
+        """ Creates the source and target widgets area
+        """
+
         # colors
         blue = QtGui.QColor(35, 140, 160)
         yellow = QtGui.QColor(250, 200, 120)
-        red = QtGui.QColor(250, 110, 90)
 
         # source widgets
         self.source_text = QtWidgets.QLineEdit()
@@ -56,9 +77,9 @@ class FlexDialog(QtWidgets.QDialog):
         self.add_target_button.setObjectName("target_qpushbutton")
         self.add_target_button.setPalette(blue)
 
-        # run widget
-        self.run_button = QtWidgets.QPushButton('--> Update shapes <--')
-        self.run_button.setPalette(red)
+    def layout_widgets(self):
+        """ Adds the widgets to the layout
+        """
 
         # adds widgets to layout
         self.grid_layout.addWidget(self.source_text, 0, 0, 1, 2)
