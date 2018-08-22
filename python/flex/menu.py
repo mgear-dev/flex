dd33fcb5e7404e0c640667074e779f9fa585c6b5
Jerome Drese <jerome.dr@grid-vfx.com>
2018-08-22 18:43:10 +0200

    Add Flex to mgear's menu

---
+++ python/flex/menu.py
@@ -0,0 +1,32 @@
+
+""" flex.menu
+
+Flex menu handles adding the Flex menu item inside the Maya mGear menu.
+
+:module: flex.menu
+"""
+
+# imports
+from __future__ import absolute_import
+from maya import cmds
+import mgear
+from mgear.flex.flex import Flex
+from mgear.flex.decorators import clean_instances
+
+
+def install():
+    """ Installs Flex submenu
+    """
+
+    cmds.setParent(mgear.menu_id, menu=True)
+    cmds.menuItem(divider=True)
+    cmds.menuItem(label="Flex", command=run)
+
+
+@clean_instances(object_name='flex_qdialog')
+def run(*args, **kwargs): # @UnusedVariable
+    """ Runs Flex Tool
+    """
+
+    flex = Flex()
+    flex.launch()
