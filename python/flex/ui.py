7d1cbbb2ef3300319352c8c54ff027c670efb39c
Jerome Drese <jdrese@gmail.com>
2018-08-28 11:15:57 +0200

    Add analyse ui widget

--- python/flex/ui.py
+++ python/flex/ui.py
@@ -8,6 +8,131 @@ Contains the Flex user interface
 
 # imports
 from PySide2 import QtWidgets, QtGui, QtCore
+from mgear.flex.query import get_resources_path
+
+# colours
+red = QtGui.QColor(250, 110, 90)
+green = QtGui.QColor(35, 160, 140)
+yellow = QtGui.QColor(250, 200, 120)
+blue = QtGui.QColor(35, 140, 160)
+yellow = QtGui.QColor(250, 200, 120)
+
+
+class FlexAnalyzeDialog(QtWidgets.QDialog):
+    """ The Flex analyze widgets
+
+    Flex analyze is a side by side list widget style that will allow you to
+    check which shapes matches.
+    """
+
+    def __init__(self, parent=None):
+        """ Creates all the user interface widgets
+
+        :param parent: the parent widget for the Flex dialog widget
+        :type parent: PySide2.QtWidgets
+        """
+        super(FlexAnalyzeDialog, self).__init__(parent=parent)
+
+        # sets window rules
+        self.setObjectName("flex_analyse_qdialog")
+        self.setWindowTitle("mGear: Flex analyze shapes")
+        self.setMinimumWidth(500)
+
+        # creates layout
+        layout = QtWidgets.QVBoxLayout()
+        layout.setMargin(0)
+
+        # setup table
+        self.setLayout(layout)
+        self.__create_table()
+
+        # add widgets
+        layout.addWidget(self.table_widget)
+
+    def __create_table(self):
+        """ Creates the table widget
+
+        We could use a table view on this case as well but for now I am keeping
+        if as a widget as the table is used only to represent a non changing
+        state of data.
+        """
+
+        # creates the table
+        self.table_widget = QtWidgets.QTableWidget()
+        self.table_widget.setColumnCount(5)
+        self.table_widget.setIconSize(QtCore.QSize(20, 20))
+
+        # adds headers
+        self.table_widget.setHorizontalHeaderLabels(["Source",
+                                                     "Target",
+                                                     "Type",
+                                                     "Count",
+                                                     "B-Box"])
+
+        # setup headers look and feel
+        h_header = self.table_widget.horizontalHeader()
+        h_header.setPalette(yellow)
+        h_header.setFixedHeight(21)
+        h_header.setDefaultSectionSize(40)
+        h_header.setSectionResizeMode(h_header.Stretch)
+        h_header.setSectionResizeMode(2, h_header.Fixed)
+        h_header.setSectionResizeMode(3, h_header.Fixed)
+        h_header.setSectionResizeMode(4, h_header.Fixed)
+        h_header.setSectionsClickable(False)
+
+        # hides vertical header
+        self.table_widget.verticalHeader().setVisible(False)
+
+    def add_items(self, source, target):
+        """ Handles adding items to the table widget
+
+        :param source: the source shape element
+        :type source: string
+
+        :param target: the target corresponding shape element matching source
+        :type target: string
+        """
+
+        # creates the icons
+        green_icon = QtGui.QIcon()
+        image = QtGui.QPixmap("{}/green.png".format(get_resources_path()))
+        green_icon.addPixmap(image)
+
+        red_icon = QtGui.QIcon()
+        image = QtGui.QPixmap("{}/red.png".format(get_resources_path()))
+        red_icon.addPixmap(image)
+
+        # source item
+        source_item = QtWidgets.QTableWidgetItem()
+        source_item.setTextAlignment(QtCore.Qt.AlignCenter)
+        source_item.setText(source)
+        source_item.setFlags(QtCore.Qt.ItemIsSelectable |
+                             QtCore.Qt.ItemIsEnabled)
+
+        # target item
+        target_item = QtWidgets.QTableWidgetItem()
+        target_item.setTextAlignment(QtCore.Qt.AlignCenter)
+        target_item.setText(target)
+        target_item.setFlags(QtCore.Qt.ItemIsSelectable |
+                             QtCore.Qt.ItemIsEnabled)
+
+        # type item
+        type_item = QtWidgets.QTableWidgetItem()
+        type_item.setIcon(green_icon)
+        type_item.setFlags(QtCore.Qt.ItemIsEnabled)
+
+        # type item
+        count_item = QtWidgets.QTableWidgetItem()
+        count_item.setIcon(red_icon)
+        count_item.setFlags(QtCore.Qt.ItemIsEnabled)
+
+        # insert items
+        self.table_widget.insertRow(0)
+        self.table_widget.setRowHeight(0, 19)
+        self.table_widget.setItem(0, 0, source_item)
+        self.table_widget.setItem(0, 1, target_item)
+        self.table_widget.setItem(0, 2, type_item)
+        self.table_widget.setItem(0, 3, count_item)
 
 
 class FlexDialog(QtWidgets.QDialog):
@@ -25,10 +150,11 @@ class FlexDialog(QtWidgets.QDialog):
         super(FlexDialog, self).__init__(parent=parent)
 
         # sets window rules
-        self.setObjectName('flex_qdialog')
+        self.setObjectName("flex_qdialog")
         self.setWindowTitle("mGear: Flex (rig updater)")
         self.setStyleSheet(self.__style_sheet())
         self.setMinimumWidth(350)
+        self.setMinimumHeight(100)
 
         # creates widgets
         self.layout_widgets()
@@ -55,7 +181,14 @@ class FlexDialog(QtWidgets.QDialog):
                  "QGroupBox::title{"
                  "subcontrol-origin: margin;"
                  "subcontrol-position: top center;"
-                 "padding:0px 10px;}")
+                 "padding:0px 10px;}"
+                 "QHeaderView{"
+                 "border:0px;"
+                 "padding: 0px;"
+                 "margin: 0px;}"
+                 "QTableView{"
+                 "margin:10px;"
+                 "padding:0px;}")
 
         return style
 
@@ -82,10 +215,6 @@ class FlexDialog(QtWidgets.QDialog):
         """ Creates the source and target widgets area
         """
 
-        # colours
-        blue = QtGui.QColor(35, 140, 160)
-        yellow = QtGui.QColor(250, 200, 120)
-
         # create layout
         grid_layout = QtWidgets.QGridLayout()
 
@@ -93,22 +222,30 @@ class FlexDialog(QtWidgets.QDialog):
         group_box = QtWidgets.QGroupBox()
         group_box.setTitle("MODEL GROUPS")
         group_box.setLayout(grid_layout)
+        group_box.setFixedHeight(80)
+        group_box.setMinimumHeight(0)
 
         # source widgets
         self.source_text = QtWidgets.QLineEdit()
         self.source_text.setObjectName("source_qlineedit")
         self.source_text.setPlaceholderText("source group")
+        self.source_text.setStatusTip(
+            "Type the name of your source group here")
         self.add_source_button = QtWidgets.QPushButton("<--  source")
         self.add_source_button.setObjectName("source_qpushbutton")
         self.add_source_button.setPalette(yellow)
+        self.add_source_button.setStatusTip("Add selected source group")
 
         # target widgets
         self.target_text = QtWidgets.QLineEdit()
         self.target_text.setObjectName("target_qlineedit")
         self.target_text.setPlaceholderText("target group")
+        self.target_text.setStatusTip(
+            "Type the name of your target group here")
         self.add_target_button = QtWidgets.QPushButton("<--  target")
         self.add_target_button.setObjectName("target_qpushbutton")
         self.add_target_button.setPalette(blue)
+        self.add_target_button.setStatusTip("Add selected target group")
 
         # adds widgets to layout
         grid_layout.addWidget(self.source_text, 0, 0, 1, 2)
@@ -123,10 +260,6 @@ class FlexDialog(QtWidgets.QDialog):
         """ Creates the run widgets area
         """
 
-        # colours
-        red = QtGui.QColor(250, 110, 90)
-        green = QtGui.QColor(35, 160, 140)
-
         # create layout
         vertical_layout = QtWidgets.QVBoxLayout()
 
@@ -134,14 +267,18 @@ class FlexDialog(QtWidgets.QDialog):
         group_box = QtWidgets.QGroupBox()
         group_box.setTitle("RUN")
         group_box.setLayout(vertical_layout)
+        group_box.setFixedHeight(80)
 
         # analyse widget
         self.analyse_button = QtWidgets.QPushButton("Analyze")
         self.analyse_button.setPalette(green)
+        self.analyse_button.setStatusTip(
+            "Hit to analyze the source and target shapes")
 
         # run widget
         self.run_button = QtWidgets.QPushButton("--> Update shapes <--")
         self.run_button.setPalette(red)
+        self.run_button.setStatusTip("Hit to execute updating your rig shapes")
 
         # adds widget to layout
         vertical_layout.addWidget(self.analyse_button)
@@ -160,6 +297,7 @@ class FlexDialog(QtWidgets.QDialog):
         group_box = QtWidgets.QGroupBox()
         group_box.setTitle("OPTIONS")
         group_box.setLayout(grid_layout)
+        group_box.setMinimumHeight(0)
 
         # user defined attributes
         self.user_attributes_check = QtWidgets.QCheckBox("User Attributes")
