1f9712e7d3abbe6e34983b23b709045ad02e95cc
Jerome Drese <jdrese@gmail.com>
2018-09-10 09:46:39 +0200

    Move logger to a common place

--- python/flex/__init__.py
+++ python/flex/__init__.py
@@ -9,3 +9,9 @@ Flex is the mGear models (geometry) update tool inside rigs.
 __author__ = 'Jerome Drese'
 __email__ = 'jeromed@gmail.com'
 __version__ = '1.0.1'
+
+# imports
+import logging
+
+logger = logging.getLogger("mGear.flex")
+logger.setLevel(logging.DEBUG)
