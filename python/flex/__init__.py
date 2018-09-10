9f1d278229641d1068e3ea009dc592655361c60b
Jerome Drese <jdrese@gmail.com>
2018-09-10 16:35:27 +0200

    Move import up

--- python/flex/__init__.py
+++ python/flex/__init__.py
@@ -6,12 +6,12 @@ Flex is the mGear models (geometry) update tool inside rigs.
 :module: flex.__init__
 """
 
-__author__ = 'Jerome Drese'
-__email__ = 'jeromed@gmail.com'
-__version__ = '1.0.1'
-
 # imports
 import logging
+
+__author__ = 'Jerome Drese'
+__email__ = 'jeromed@gmail.com'
+__version__ = '1.0.2'
 
 logger = logging.getLogger("mGear.flex")
 logger.setLevel(logging.DEBUG)
